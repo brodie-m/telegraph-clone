03660d047da331de37f63d46ea25bbd88f333992
Callum <80120239+cmr33-cloud@users.noreply.github.com>
2021-09-28 17:05:32 +0100

    prepare to merge

--- api/server.js
+++ api/server.js
@@ -9,55 +9,56 @@ server.use(express.json());
 
 // function newPost(title, author, body) {}
 function newPost(body) {
-console.log(body)
+  console.log(body);
   return new Post({
-    "title": `${body.title}`,
-    "author": `${body.author}`,
-    "body": `${body.body}`,
-  })
-    
+    title: body.title,
+    author: body.author,
+    body: body.body,
+  });
 }
 
 server.get("/", (req, res) => res.send("hello world"));
 server.post("/add-post", (req, res) => {
-//   newPost(req.body.title, req.body.name, req.body.entry);
-  const postToAdd = newPost(req.body)
-  postToAdd.save()
-    .then((result) => {
-      res.send(result);
+  //   newPost(req.body.title, req.body.name, req.body.entry);
+  newPost(req.body)
+    .save()
+    .then((res) => {
+      res.send({ msg: "Post successfully added!" });
     })
     .catch((err) => {
       console.log(err);
     });
 });
 
-server.get('/all-posts', (req, res) => {
-    Post.find()
-        .then(result => {
-            res.send(result)
-        })
-        .catch((error)=> {
-            console.log(error)
-        })
-})
+server.get("/all-posts", (req, res) => {
+  Post.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
-server.get('/single-post',(req,res) => {
-    Post.findById(`615321dc912643518bd1828d`)
-    .then(result => {
-        res.send(result)
+server.get("/single-post", (req, res) => {
+  Post.findById(`615321dc912643518bd1828d`)
+    .then((result) => {
+      res.send(result);
     })
-    .catch((error)=> {
-        console.log(error)
-    })
-})
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 //connect to mongoDB
-const dbURI = "mongodb+srv://db-admin:db-password-1234@cluster0.t1ajl.mongodb.net/posts-db?retryWrites=true&w=majority"
-mongoose.connect(dbURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then((result) => console.log('connected to db'))
-    .catch((err) => console.log(err))
+const dbURI =
+  "mongodb+srv://db-admin:db-password-1234@cluster0.t1ajl.mongodb.net/posts-db?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => console.log("connected to db"))
+  .catch((err) => console.log(err));
 
 module.exports = server;
