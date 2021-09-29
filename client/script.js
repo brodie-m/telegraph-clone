8835195ba06fdbe8aff938c369f3cba6d2e97263
brodie-m <brodiedmcguire@gmail.com>
2021-09-29 11:52:08 +0100

    trying to make post redirect work

--- client/script.js
+++ client/script.js
@@ -1,9 +1,11 @@
 const host = "fp-telegraph-clone.herokuapp.com"
 const clientURL = window.location.href
-const clientRouter = require('./routing')
+const clientURLArray = clientURL.split('/')
 const title = document.getElementById('title')
 const pseudo = document.getElementById('pseudo');
 const entry = document.getElementById('entry');
+
+
 
 
 addentry.addEventListener("click", async () => {
@@ -37,12 +39,12 @@ async function redirectToPost() {
   const allPosts = await (await fetch(`https://${host}/all-posts`)).json()
   const recentPost = await allPosts[allPosts.length-1]
   const recentPostId = await recentPost._id;
-  console.log(recentPost,recentPostId)
-  console.log(clientURL)
-  window.location.href = `./html/single-post.html`
-  console.log(clientURL)
+  window.location.href = `./#single-post-${recentPostId}`
+  
   
 }
+
+
 
 revealall.addEventListener("click", (e) => {
   e.preventDefault();
@@ -53,3 +55,33 @@ cancel.addEventListener("click", (e) => {
   newentry.hidden = true;
   for(let o of inputs.children){o.value = ""}
 });
+
+
+function renderPost() {
+  const hash = window.location.hash
+  const hashArr = hash.split('-')
+  const postId = hashArr[hashArr.length-1]
+  getPostData(postId)
+}
+
+async function getPostData(postId) {
+  const data = await fetch(`https://${host}/single-post/${postId}`)
+  const json = await data.json()
+  renderSinglePost(json);
+}
+function renderSinglePost(data) {
+  const container = document.createElement('div')
+  const postTitle = document.createElement('h1')
+  const postAuthor = document.createElement('h2')
+  const postBody = document.createElement('p')
+  container.appendChild(postTitle)
+  container.appendChild(postAuthor)
+  container.appendChild(postBody)
+  postTitle.textContent = data.title;
+  postAuthor.textContent = data.author;
+  postBody.textContent = data.body;
+
+}
+
+
+
---
+++ client/single-post.js
@@ -0,0 +1,1 @@
+const postData = fetch()
